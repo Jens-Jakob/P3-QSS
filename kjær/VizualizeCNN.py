eb778494b7c9c82a510e14e9b97d38ac21ec06fa
T4t00N <144425596+T4t00N@users.noreply.github.com>
2024-10-08 18:49:30 +0200

    Delete kjær directory

--- kjær/VizualizeCNN.py
+++
@@ -1,7 +0,0 @@
-#guide https://github.com/TylerYep/torchinfo
-from torchinfo import summary
-import feature_extractor
-
-#Modellen her!
-model = feature_extractor.build_feature_extractor()
-summary(model, (1, 3, 224, 224), depth=3)
